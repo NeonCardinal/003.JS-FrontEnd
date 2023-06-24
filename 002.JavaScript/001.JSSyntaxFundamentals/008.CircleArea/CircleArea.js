5cbd9b7173583cddf562122ce1993cdc435476ce
NeonCardinal <nvnnaidenov@gmail.com>
2023-06-24 11:34:00 +0300

    add

---
+++ 002.JavaScript/001.JSSyntaxFundamentals/008.CircleArea/CircleArea.js
@@ -0,0 +1,18 @@
+function CalculateCircleArea(radius)
+{
+    let result;
+    let inputType = typeof(radius);
+
+    if(inputType == 'number')
+    {
+        result = Math.pow(radius, 2) * Math.PI;
+
+        console.log(result.toFixed(2));
+    }
+    else
+    {
+        console.log(`We can not calculate the circle area, because we receive a ${inputType}`);
+    }
+}
+
+CalculateCircleArea("test");
